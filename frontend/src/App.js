6b14a2a801fa17cd96c0c73cf6acd323cfc6aede
PremiumDelevent <jofre.coll@premiumdelevent.com>
2025-09-10 13:07:50 +0200

    :bulb: UPDATE: render components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,22 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+import Home from './Home';
+import Intercambios from './Intercambios';
 
 function App() {
-  const [salesLines, setSalesLines] = useState([]);
+  const [currentPage, setCurrentPage] = useState('home');
 
-  useEffect(() => {
-    fetch("http://localhost:4000/api/sales-lines")
-      .then(res => res.json())
-      .then(data => {
-        // data.salesLines es un array de arrays
-        const allLines = data.salesLines.flat();
-        setSalesLines(allLines);
-        console.log(data);
-      })
-      .catch(err => console.error(err));
-  }, []);
+  const renderPage = () => {
+    switch(currentPage) {
+      case 'intercambios':
+        return <Intercambios onNavigate={setCurrentPage} />;
+      default:
+        return <Home onNavigate={setCurrentPage} />;
+    }
+  };
 
   return (
-    <div className="container-1">
-      <h1>WMS PREMIUM DELEVENT</h1>
-      <p>Welcome to the WMS PREMIUM DELEVENT application!</p>
-      <p>Check the console for the API response.</p>
-
-      <table border="1" cellPadding="5" cellSpacing="0">
-        <thead>
-          <tr>
-            <th>Location Code</th>
-            <th>Shortcut Dimension 1 Code</th>
-            <th>Document No</th>
-            <th>Description</th>
-          </tr>
-        </thead>
-        <tbody>
-          {salesLines
-          .filter(line => line.Location_Code.slice(2) !== line.Shortcut_Dimension_1_Code)
-          .map((line, index) => (
-            <tr key={index}>
-              <td>{line.Location_Code}</td>
-              <td>{line.Shortcut_Dimension_1_Code}</td>
-              <td>{line.Document_No}</td>
-              <td>{line.Description}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      {renderPage()}
     </div>
   );
 }
