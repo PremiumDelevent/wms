b8eced5f1a595d48515dacb54c5420106664542b
PremiumDelevent <jofre.coll@premiumdelevent.com>
2025-09-12 11:51:32 +0200

    :bulb: UPDATE: fetch api products

--- frontend/src/Stock.js
+++ frontend/src/Stock.js
@@ -1,5 +1,17 @@
+import { useEffect, useState } from "react";
+
 
 function Stock({ onNavigate }) {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+      fetch("http://localhost:4000/api/products")
+        .then(res => res.json())
+        .then(data => {
+          console.log(data);
+          const allProducts = data.products.flat();
+          setProducts(allProducts);
+        })
+    }, []);
 
   return (
     <div className="container-1">
@@ -22,6 +34,28 @@ function Stock({ onNavigate }) {
         <h1>Stock - WMS PREMIUM DELEVENT</h1>
 
         <p>Esta es la página de Stock.</p>
+
+        <table border="1" cellPadding="5" cellSpacing="0">
+          <thead>
+            <tr>
+              <th>Display Name</th>
+              <th>Number</th>
+              <th>Category</th>
+              <th>Stock</th>
+            </tr>
+          </thead>
+          <tbody>
+            {products
+              .map((product, index) => (
+                <tr key={index}>
+                  <td>{product.displayName}</td>
+                  <td>{product.number}</td>
+                  <td>{product.itemCategoryCode}</td>
+                  <td>0</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
 
     </div>
   );
